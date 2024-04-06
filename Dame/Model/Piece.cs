1ad53b1e563ab39658883a70a76206fbda051d06
AVPavel <110391560+AVPavel@users.noreply.github.com>
2024-04-06 22:36:31 +0300

    Added folders for structure and models

---
+++ Dame/Model/Piece.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dame.Model
+{
+    public class Piece
+    {
+        public enum Color { White, Red }
+        public Color PieceColor { get; set; }
+        public bool isKing { get; set; }
+        public (int row, int column) Position { get; set; }
+
+        public Piece(Color color, int row, int column)
+        {
+            this.PieceColor = color;
+            this.isKing = true;
+            this.Position = (row, column);
+        }
+    }
+}
